b8672efb414a28a9bf709ad1216f359f7cfb7a76
naty-nog <nogalesgarcianatalysusca@gmail.com>
2026-09-11 12:41:44 -0400

    modelo usuario

---
+++ PixMarket/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace PixMarket.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Correo { get; set; }
+        public string? Contraseña { get; set; }
+        public string? Rol { get; set; }
+    }
+}
