a0f1c92607990be072a4d496c93dc5fc71ef698a
asdaf987654321-dot <asdaf987654321@gmail.com>
2026-09-11 10:45:22 -0400

    Agregar archivos de proyecto.

---
+++ PixMarket/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PixMarket.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
