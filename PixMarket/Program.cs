50d80defda6b1bc19756f64bc9ddadf6510bc978
asdaf987654321-dot <asdaf987654321@gmail.com>
2026-09-12 00:48:20 -0400

    registrar PixMarket y conecion a MySQL

--- PixMarket/Program.cs
+++ PixMarket/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PixMarket.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddDbContext<PixContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
