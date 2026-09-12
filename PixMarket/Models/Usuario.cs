e0020eab97d86b2a3ef0960ddd94711524331120
naty-nog <nogalesgarcianatalysusca@gmail.com>
2026-09-12 00:22:09 -0400

    home controller y creacion de data y pixContext

--- PixMarket/Models/Usuario.cs
+++ PixMarket/Models/Usuario.cs
@@ -7,7 +7,7 @@ namespace PixMarket.Models
         public int Id { get; set; }
         public string? Nombre { get; set; }
         public string? Correo { get; set; }
-        public string? Contraseña { get; set; }
+        public string? Contrasenia { get; set; }
         public string? Rol { get; set; }
     }
 }
