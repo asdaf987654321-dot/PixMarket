b44938b1f8377b7bbc7f3012f7d93c4ca841dd02
Gabriel Fidel Thokokary Jaldin <thokokaryjaldingabriel@gmail.com>
2026-09-19 00:16:49 +0200

    creación de los modelos de Venta, DetalleVenta e Item, implementacion de Data annotation en el modelo de Usuario

---
+++ PixMarket/Models/DetalleVenta.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PixMarket.Models
+{
+    public class DetalleVenta
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int IdVenta { get; set; }
+
+        [Required]
+        public int IdItem { get; set; }
+
+        [Required(ErrorMessage = "La cantidad es obligatoria")]
+        public int Cantidad { get; set; }
+
+        [Required(ErrorMessage = "El precio unitario es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El Precio unitario debe ser mayor que cero")]
+        public decimal PrecioUnitario { get; set; }
+
+        [Required(ErrorMessage = "El subtotal es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El Subtotal debe ser mayor que cero")]
+        public decimal Subtotal { get; set; }
+
+        [ForeignKey(nameof(IdVenta))]
+        public Venta Venta { get; set; } = null!;
+
+        [ForeignKey(nameof(IdItem))]
+        public Item Item { get; set; } = null!;
+    }
+}
---
+++ PixMarket/Models/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PixMarket.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id{ get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "El nombre debe tener entre 1 y 150 caracteres")]
+        public string? Nombre { get; set; }
+
+        [StringLength(500)]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor que cero")]
+        public decimal Precio { get; set; }
+
+        public int Stock { get; set; }
+
+        public ICollection<DetalleVenta> DetallesVenta { get; set; }
+            = new List<DetalleVenta>();
+    }
+}
