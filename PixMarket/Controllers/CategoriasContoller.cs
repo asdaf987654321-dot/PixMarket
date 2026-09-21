74525fb829dbba14f343f60a511aba86b799aaed
Kirito-creador <leandromartinezreyes657@mail.com>
2026-09-20 22:53:38 -0400

    Cambio momentaneo de la tienda

--- PixMarket/Controllers/CategoriasContoller.cs
+++ PixMarket/Controllers/CategoriasContoller.cs
@@ -4,7 +4,14 @@ namespace PixMarket.Controllers
 {
     public class CategoriasController : Controller
     {
+        // Vista de TIENDA
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Vista de CATEGORÍAS
+        public IActionResult Categorias()
         {
             return View();
         }
