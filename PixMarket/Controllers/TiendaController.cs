db1de0266a23417d19a4ed56ce25d840769c214e
naty-nog <nogalesgarcianatalysusca@gmail.com>
2026-09-20 21:30:31 -0400

    Controller tienda agregado

---
+++ PixMarket/Controllers/TiendaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PixMarket.Controllers
+{
+    public class TiendaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
